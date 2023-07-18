ffcf8e9f5e801908c5af9262d89e27edb5dfb227
Adomas Daskevicius <adomasda@gmail.com>
2023-07-18 19:49:17 +0300

    Documentation phase

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import lt.auskim.TextProcessors;
+import lt.auskim.utils.TextProcessorMapper;
 import lt.auskim.utils.TextProcessorsInput;
 
 public class Main {
@@ -8,6 +9,11 @@ public class Main {
 
         TextProcessorsInput input = new TextProcessorsInput(textProcessors, "input.txt");
         input.deleteAllOutputFiles();
+
+//      Process all
         input.processAll();
+
+//      Process one
+//        input.process(TextProcessors.GroupedProcessor.class);
     }
 }
